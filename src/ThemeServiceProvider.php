d4ecd0464de808af0bb7e3266567aa3d5d6ce2a9
hacoidev <hacoi.dev@gmail.com>
2022-07-16 09:53:22 +0700

    init theme

---
+++ src/ThemeServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ophim\Theme\VietPhimTV;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        config(['ophim.themes' => array_merge(config('ophim.themes', []), [
+            'vietphimtv' => 'VietphimTV'
+        ])]);
+    }
+
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/theme', 'ophim::themes.vietphimtv');
+    }
+}
